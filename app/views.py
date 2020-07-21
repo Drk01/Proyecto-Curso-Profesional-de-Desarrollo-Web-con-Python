badfbe45a95c878276f2d07798570f7335997ad0
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-21 18:28:23 -0500

    Cambiando los .jinja por .html
    Esto se hizo porque algunas funciones no respondían como se esperaba

--- app/views.py
+++ app/views.py
@@ -7,12 +7,12 @@ page = Blueprint('page', __name__)
 
 @page.app_errorhandler(404)
 def page_not_found(error):
-    return render_template('errors/404.jinja'), 404
+    return render_template('errors/404.html'), 404
 
 
 @page.route('/')
 def index():
-    return render_template('index.jinja')
+    return render_template('index.html')
 
 
 @page.route('/login')
