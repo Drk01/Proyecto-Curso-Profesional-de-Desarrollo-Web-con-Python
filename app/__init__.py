9a8a1874f585a4e579e23aa2382ac9e61aeb7879
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-21 08:36:56 -0500

    Implementando Flask-Bootstrap4 en el sitio

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from .views import page
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap()
 
 
 def createApp(config):
     app.config.from_object(config)
+    bootstrap.init_app(app)
     app.register_blueprint(page)
     return app
