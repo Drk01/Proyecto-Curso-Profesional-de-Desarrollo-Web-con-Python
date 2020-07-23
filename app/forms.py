621e5ad68c0dfe7a32dd69efdd3ad5b8eb80568d
drk01 <WFkXSFepPQz7Ff>
2020-07-22 21:02:38 -0500

    Generar contraseña

--- app/forms.py
+++ app/forms.py
@@ -1,33 +1,48 @@
 from wtforms import Form, StringField, PasswordField, validators, BooleanField
 from wtforms.fields.html5 import EmailField
 import email_validator
+from werkzeug.security import generate_password_hash
 
 
 class LoginForm(Form):
-    username = StringField('Username: ', [
-        validators.length(
-            min=4, max=50, message='El username se encuentra fuera de rango'),
-        validators.Required()])
-    password = PasswordField('Password: ', [
-        validators.Required(message='La contraseña es requerida')
-    ])
+    username = StringField(
+        "Username: ",
+        [
+            validators.length(
+                min=4, max=50, message="El username se encuentra fuera de rango"
+            ),
+            validators.Required(),
+        ],
+    )
+    encrypted_password = PasswordField(
+        "Password: ", [validators.Required(message="La contraseña es requerida")]
+    )
+
+    @property
+    def password(self):
+        pass
+
+    @password.setter
+    def password(self, value):
+        self.encrypted_password = generate_password_hash(value)
 
 
 class RegisterForm(Form):
-    username = StringField('Username', [
-        validators.length(min=4, max=50)
-    ])
-    email = EmailField('Correo electrónico', [
-        validators.length(min=6, max=100),
-        validators.Required(message='El email es requerido'),
-        validators.Email(message='Ingrese un email válido')
-    ])
-    password = PasswordField('Password', [
-        validators.Required(message='El password es requerido'),
-        validators.EqualTo('confirm_password',
-                           message='La contraseña no coíncide')
-    ])
-    confirm_password = PasswordField('Confirmar contraseña')
-    accept = BooleanField('', [
-        validators.DataRequired()
-    ])
+    username = StringField("Username", [validators.length(min=4, max=50)])
+    email = EmailField(
+        "Correo electrónico",
+        [
+            validators.length(min=6, max=100),
+            validators.Required(message="El email es requerido"),
+            validators.Email(message="Ingrese un email válido"),
+        ],
+    )
+    password = PasswordField(
+        "Password",
+        [
+            validators.Required(message="El password es requerido"),
+            validators.EqualTo("confirm_password", message="La contraseña no coíncide"),
+        ],
+    )
+    confirm_password = PasswordField("Confirmar contraseña")
+    accept = BooleanField("", [validators.DataRequired()])
