4fdbe0bb57d6df0ff0413def6446e06e93971baf
drk01 <WFkXSFepPQz7Ff>
2020-07-22 20:45:13 -0500

    Registrando usuario

--- app/views.py
+++ app/views.py
@@ -1,34 +1,45 @@
 from flask import Blueprint
 from flask import render_template, request
 from .forms import LoginForm, RegisterForm
+from .models import User
 
-page = Blueprint('page', __name__)
+page = Blueprint("page", __name__)
 
 
 @page.app_errorhandler(404)
 def page_not_found(error):
-    return render_template('errors/404.html'), 404
+    return render_template("errors/404.html"), 404
 
 
-@page.route('/')
+@page.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@page.route('/login', methods=['GET', 'POST'])
+@page.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm(request.form)
 
-    if request.method == 'POST' and form.validate():
+    if request.method == "POST" and form.validate():
         print(form.username.data)
         print(form.password.data)
 
-        print('Nueva sesión creada')
+        print("Nueva sesión creada")
 
-    return render_template('auth/login.html', title='Login', form=form)
+    return render_template("auth/login.html", title="Login", form=form)
 
 
-@page.route('/register')
+@page.route("/register", methots=["GET", "POST"])
 def register():
-    form = RegisterForm()
-    return render_template('auth/register.html', title='Registro', form=form)
+    form = RegisterForm(request.form)
+
+    if request.method == "POST":
+        if form.validate():
+            user = User.create_element(
+                form.username.data, form.password.data, form.email.data
+            )
+
+            print("Usuario creado de forma exitosa", user.id)
+
+    return render_template("auth/register.html", title="Registro", form=form)
+
